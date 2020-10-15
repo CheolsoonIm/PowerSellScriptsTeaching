757675e8c1f94d9894dc4ce443d0c80a92cc7858
Campanis <W0038182@campus.nscc.ca>
2020-10-15 15:42:23 -0300

    int

---
+++ GroupAIntro.ps1
@@ -0,0 +1,17 @@
+<#
+my directory: "C:\Users\w0038182\OneDrive - Nova Scotia Community College\Classes\Fall2020\INET - Server Scripting\GroupB\PSScripts"
+F8 - runs selected text
+F5 - runs all text
+ctrl C will escape
+use up and down arrows to scroll between prev commands
+use tab in terminal to scroll/autocomplete e.g Get- <tab>
+Esc removes current line
+Use * as wildcards
+
+use > to save/redirect
+use >> to append
+#>
+
+Get-Process
+clear
+
